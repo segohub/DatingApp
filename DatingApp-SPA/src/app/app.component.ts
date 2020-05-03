009478c539d44808ca293242608d1a76b9d385a0
GOSSOU <segohub@gmail.com>
2020-05-03 19:42:31 +0200

    End of Section 14

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -17,10 +17,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const token = localStorage.getItem('token');
     const user: User = JSON.parse(localStorage.getItem('user'));
-    if(token) {
+    if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
-    if(user) {
+    if (user) {
       this.authService.currentUser = user;
       this.authService.changeMemberPhoto(user.photoUrl);
     }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, PaginationModule } from 'ngx-bootstrap';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
@@ -72,6 +72,7 @@ export function tokenGetter() {
       TabsModule.forRoot(),
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
+      PaginationModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
          config: {
